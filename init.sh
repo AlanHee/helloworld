9f0d96d1eb0d20f30348b91bc9929b3be9ee84c7
AlanHee <alan.hee@outlook.com>
2021-01-11 20:40:23 +0800

    Added base cli

--- init.sh
+++ init.sh
@@ -1,21 +1,33 @@
-# author: AlanHee
-# email: alan.hee@outlook.com
-# site: www.alanhee.site
+# author: 	AlanHee
+# email: 	alan.hee@outlook.com
+# home page: 	www.alanhee.site
 
-# workflow:
-# 1 echo hello world
-# 2 ask your env
-# 2.1 install helpful tools: openssl nodejs git
-# 2.2 determine if installed tools echo result 
-# 3 enter github username&pwd
-# 4 pull github respos
+echo "Hello ..."
 
-echo "Hello World."
+## Install tools
 apt update
 apt install -y git zsh nodejs openssl 
 
-## config git
-#  git config --global user.email "you@example.com"
-#  git config --global user.name "Your Name"
+## Config git
+gitUserName='';
+while true; do
+    read -p 'Enter your git global name:' gitName;
+    if [ -n "$gitName" ]; then
+	    git config --global user.name "$gitName";
+	    gitUserName=$gitName;
+	    break;
+    fi
+done
+while true; do
+    read -p 'Enter your git global email:' gitEmail;
+    if [ -n "$gitEmail" ]; then
+	    git config --global user.email "$gitEmail";
+	    break;
+    fi
+done
 
-echo "Done. Good job man!"
+## Pull repos
+git clone https://github.com/"$gitUserName"/blog
+git clone https://github.com/"$gitUserName"/cookbook
+
+echo "Hello World!"
