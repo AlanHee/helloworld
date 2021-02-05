616ab1f25fcc2089def50dfec77d435fadb9fd02
AlanHee <alan.hee@outlook.com>
2021-02-05 11:43:30 +0800

    Added alias

--- src/set-alias.sh
+++ src/set-alias.sh
@@ -6,3 +6,6 @@ echo "alias cd.blog='cd ~/blog'" >> ~/.zshrc
 echo "alias book.cli='vim ~/cookbook/profession/cli.md'" >> ~/.zshrc
 echo "alias book.vim='vim ~/cookbook/profession/vim.md'" >> ~/.zshrc
 echo "alias book.git='vim ~/cookbook/profession/git.md'" >> ~/.zshrc
+echo "alias open.zsh='vim ~/.zshrc'" >> /.zshrc
+echo "alias open.vim='vim ~/.vimrc'" >> /.zshrc
+
