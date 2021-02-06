eaf55ab3ec5a378d56d5e08b84669c0bde0c8f26
Alan Hee <alan.hee@outlook.com>
2021-02-06 11:49:53 +0800

    Fixed mv vim rc issue.

--- src/set-vim.sh
+++ src/set-vim.sh
@@ -2,13 +2,13 @@ apt install vim
 
 if [ -e ~/.vim/bundle/Vundle.vim ];
 then
-	mv -f ~/.vim/bundle/Vundle.vim ~/.vim/bundle/Vundle.vim.old;
+	mv -f ~/.vim/bundle/Vundle.vim ~/.vim/bundle/Vundle.vim.old.$(date +%Y.%m.%d-%H:%M:%S);
 fi
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 if [ -e ~/.vimrc ];
 then
-	mv -f ~/.vimrc ~/.vimrc.old;
+	mv -f ~/.vimrc ~/.vimrc.old.$(date +%Y.%m.%d-%H:%M:%S);
 fi
 cp $SRCHOME/.vimrc ~/.vimrc
 
