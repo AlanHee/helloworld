416a68d367b7899be3ea9f5e920a097713bee71b
Alan Hee <alan.hee@outlook.com>
2021-02-06 11:56:00 +0800

    Fixed alias issue

--- src/set-alias.sh
+++ src/set-alias.sh
@@ -1,7 +1,7 @@
 echo "alias m='mans'" >> ~/.zshrc
 echo "alias v='vim'" >> ~/.zshrc
 echo "alias cd.book='cd ~/cookbook'" >> ~/.zshrc
-echo "alias cd.src='cd $SRCHOME" >> ~/.zshrc
+echo "alias cd.src='cd $SRCHOME'" >> ~/.zshrc
 echo "alias cd.blog='cd ~/blog'" >> ~/.zshrc
 echo "alias book.cli='vim ~/cookbook/profession/cli.md'" >> ~/.zshrc
 echo "alias book.vim='vim ~/cookbook/profession/vim.md'" >> ~/.zshrc
