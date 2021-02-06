a7ac8dae506e77f9e4d30c372453b285f75cd4f6
Alan Hee <alan.hee@outlook.com>
2021-02-06 11:25:38 +0800

    Added set-vim

--- src/set-vim.sh
+++ src/set-vim.sh
@@ -1,1 +1,15 @@
-echo "sets vim"
+apt install vim
+
+if [ -e ~/.vim/bundle/Vundle.vim ];
+then
+	mv -f ~/.vim/bundle/Vundle.vim ~/.vim/bundle/Vundle.vim.old;
+fi
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+if [ -e ~/.vimrc ];
+then
+	mv -f ~/.vimrc ~/.vimrc.old;
+fi
+cp $SRCHOME/.vimrc ~/.vimrc
+
+echo "setup vim."
