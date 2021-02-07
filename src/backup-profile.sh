81627c46229db7652cac63f90e5431cf091fdfbc
Alan Hee <alan.hee@outlook.com>
2021-02-07 12:21:09 +0800

    Modified vimrc & zshrc

---
+++ src/backup-profile.sh
@@ -0,0 +1,5 @@
+mv -f .zshrc .zshrc.old
+mv -f .vimrc .vimrc.old
+cp -f ~/.zshrc .
+cp -f ~/.vimrc .
+echo "backup .zshrc & .vimrc";
---
+++ src/set-ycm.sh
@@ -0,0 +1,5 @@
+# set vim plugin youcompleteme
+apt upgrade
+apt install cmake vim-python
+python3 ~/.vim/bundle/youcompleteme/install.py --clang-completer
+echo "setup vim youcompleteme plugin."
