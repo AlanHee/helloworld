1fdf419e4d4c90a9eb156f5ca63f37d6a50f68c4
Alan Hee <alan.hee@outlook.com>
2023-04-28 02:15:20 +0800

    Added hello-termux.sh

---
+++ src/add-bash-bang.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Replace "directory_path" with the path of the directory containing the files you want to modify
+DIRECTORY="/data/data/com.termux/files/home/bin/src"
+
+# Replace "line_of_text" with the line of text you want to add to each file
+LINE="#!/bin/bash"
+
+# Loop through each file in the directory
+for file in $DIRECTORY/*; do
+  # Use sed to insert the line of text at the beginning of the file
+  # TODO 
+  #sed -i "$LINE" "$file"
+  # Print a message indicating which file was modified
+  echo "Added line to $file"
+done
