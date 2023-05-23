792b7718a23f8acc24b6bf507c2f169d5470d219
Alan Hee <alan.hee@outlook.com>
2023-05-24 04:30:01 +0800

    Modified hello-ubuntu

--- src/hello-ubuntu.sh
+++ src/hello-ubuntu.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-
 echo 'hello ubuntu!';
+scripts=(
+	ubuntu-get-tools
+	neovim-config-plugs
+	ubuntu-get-dart
+	ubuntu-get-dart-frog
+	git-global-config
+	git-auto-auth
+	ssh-key-gen
+	ubuntu-get-bt-panel
+	ubuntu-get-vpn-server
+	ubuntu-config-ports
+)
 
-#termux-reload-settings
+for script in ${scripts[@]}
+do
+  bash $script.sh
+done
---
+++ src/ubuntu-get-tools.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo 'ubuntu get tools';
