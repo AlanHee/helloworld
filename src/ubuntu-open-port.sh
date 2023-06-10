d54523376820f46b69a14ae5a91cbe3337884a13
Alan Hee <alan.hee@outlook.com>
2023-06-10 21:19:33 +0000

    Added open close port script

---
+++ src/ubuntu-open-port.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -e /usr/sbin/ufw ]; then	
+	sudo apt-get update -y
+	sudo apt-get install ufw -y
+fi
+
+if sudo ufw status | grep -q "$1"; then
+    echo "$1 port has open"
+else
+    sudo ufw allow $1/tcp
+    echo "success open port $1"
+fi
+
--- src/ubuntu-set-ports.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo 'set ports todo';
