45a5cff9424a6b87a8652921b34a8ffb1b0430db
Alan Hee <alan.hee@outlook.com>
2023-07-19 12:49:31 +0800

    Modified ssh-push-pub.sh

--- src/ssh-push-pub.sh
+++ src/ssh-push-pub.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 while true; do
-	read -p "Type in romote IP address:" ssh_ip
+	read -p "Type in remote ip (default `echo $REMOTE_IP`) : " ssh_ip
 	if [ -z $ssh_ip ]; 
 	then
-		echo "Ip address can not be empty.";
+		if [ ! -z $REMOTE_IP ]; then
+			ssh root@$REMOTE_IP "cat >> ~/.ssh/authorized_keys" < ~/.ssh/id_rsa.pub
+			break;
+		else
+			echo "Ip address can not be empty.";
+		fi
 	else 
 		ssh root@$ssh_ip "cat >> ~/.ssh/authorized_keys" < ~/.ssh/id_rsa.pub
 		break;
