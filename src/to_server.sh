6a29362c0c4628e07ff34b9dc50694fac8e1ab03
Alan Hee <alan.hee@outlook.com>
2023-07-19 13:55:30 +0800

    Modified to_server.sh

--- src/to_server.sh
+++ src/to_server.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-#TODO 
-ssh root@$SERVER_IP 
+if [ ! -z $REMOTE_IP ]; then
+	ssh root@$REMOTE_IP 
+else 
+	while true; do
+		read -p 'type in remote ip:' remote_ip
+		if [ ! -z $remote_ip ]; then
+			ssh root@$remote_ip 
+			break;
+		else 
+			echo 'remote ip can be empty.'
+		fi
+	done
+fi
