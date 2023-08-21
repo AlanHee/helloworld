755603a671d23c85cce32f25ca24ebd914bd175a
Alan Hee <alan.hee@outlook.com>
2023-08-22 03:44:47 +0800

    Mofified hello.sh

--- hello.sh
+++ hello.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-# get sciprts
+# load sciprts
 fold=bin
 while true; do
     read -p 'enter script fold:' funsName;
-	fold=$funsName;
-    if [ -e ~/$fold ]; then
+    if [ -e ~/$funsName ]; then
 	    echo "fold existed!"; 
 		break;
     else
+		if [ ! -z $funsName ]; then
+			fold=$funsName;
+		fi
 		git clone https://github.com/alanhee/helloworld ~/$fold;
 	    break;
     fi
-
-	# load funs
-	source ~/$fold/functions.sh
-	
-	# save vars
-	setVar PATH  $PATH:~/$fold/src
-	setVar SRCHOME ~/$fold/src
-	setVar PROFILE $~/$fold/profile
-
-	# determine if termux or...
-	if [ "$(uname -o)" = "Android" ]; then 
-		bash hello-termux.sh
-	else
-		bash hello-ubuntu.sh
-	fi
 done
+
+# expord funs
+source ~/$fold/functions.sh
+	
+# save vars
+setVar PATH  $PATH:~/$fold/src
+setVar SRCHOME ~/$fold/src
+setVar PROFILE $~/$fold/profile
+
+# determine env
+if [ "$(uname -o)" = "Android" ]; then 
+	bash hello-termux.sh
+else
+	bash hello-ubuntu.sh
+fi
