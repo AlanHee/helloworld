e6ce0a690447617990d3e467c747ce1e6451f848
Alan <alan.hee@outlook.com>
2023-08-24 03:19:22 +0800

    Update config-profile-settings.sh

--- src/config-profile-settings.sh
+++ src/config-profile-settings.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 read -p "Type in your name: " user_name 
-setVar USER_NAME $user_name
+saveConst USER_NAME $user_name
 read -p "Type in your email: " user_email
-setVar USER_EMAIL $user_email
+saveConst USER_EMAIL $user_email
 
 source ~/.bashrc
