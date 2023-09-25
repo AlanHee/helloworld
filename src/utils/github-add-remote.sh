46cd2b96ec1e48c3c8828ec99ec994093c579b8c
root <root@US-A-84889065.local>
2023-09-25 13:37:36 +0000

    Modified new user in ubuntu env

--- src/utils/github-add-remote.sh
+++ src/utils/github-add-remote.sh
@@ -4,7 +4,7 @@ if [ -z $repo_name ]; then
 	echo 'oop error'
 else
 	git remote remove origin
-	git remote add origin git@github.com:AlanHee/$repo_name.git
+	git remote add origin git@github.com:alanhee/$repo_name.git
 fi
 git branch -M main
 git push -u origin main
