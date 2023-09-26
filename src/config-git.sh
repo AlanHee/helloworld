eca259dbd098dee3b520bd53320329d2ec9f7364
AlanHee <alan.hee@outlook.com>
2023-09-27 02:13:26 +0800

    Modified config-git.sh

--- src/config-git.sh
+++ src/config-git.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
+
 if [[ ! -z $USER_NAME && ! -z $USER_EMAIL ]]; then
 	git config --global user.name $USER_NAME
 	git config --global user.email $USER_EMAIL
-	git config --global credential.helper store
 	echo 'git auto setup'
-	exit
+else 
+	# config user name
+	read -p "config git global user name(Alan Hee):" user_name
+	if [ -z $user_name ]; then
+		git config --global user.name "Alan Hee"
+	else
+		git config --global user.name $user_name
+	fi
+	# config user password
+	read -p "config git global user email(alan.hee@outlook.com):" user_email
+	if [ -z $user_email ]; then
+		git config --global user.email "alan.hee@outlook.com"
+	else
+		git config --global user.email $user_email
+	fi
 fi
-# config user name
-read -p "config git global user name(Alan Hee):" user_name
-if [ -z $user_name ]; then
-	git config --global user.name "Alan Hee"
-else
-	git config --global user.name $user_name
-fi
-# config user password
-read -p "config git global user email(alan.hee@outlook.com):" user_email
-if [ -z $user_email ]; then
-	git config --global user.email "alan.hee@outlook.com"
-else
-	git config --global user.email $user_email
-fi
-
-# config auto auth
 git config --global credential.helper store
