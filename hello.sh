da3dc7aa7bfb89c672d2f31aaa67d945e2e493e1
Alan Hee <alan.hee@outlook.com>
2023-11-09 19:14:03 +0800

    Updated init.sh

--- hello.sh
+++ hello.sh
@@ -3,16 +3,13 @@
 apt upgrade -qyy 
 apt install git -qyy
 
-read -p 'Please set script home path: ' home;
+read -p 'Type in a path to save hello script:' home;
 if [ -e ~/$home ]; then
-	echo "home path existed."; 
+	echo "Oop path existed."; 
 	exit 0
 fi
+
 git clone https://github.com/alanhee/helloworld ~/$home;
 
 bash ~/$home/src/init.sh $home		
-if [ "$(uname -o)" = "Android" ]; then 
-	bash ~/$home/src/hello-termux.sh
-else
-	bash ~/$home/src/hello-ubuntu.sh
-fi	
+
--- src/init.sh
+++ src/init.sh
@@ -1,4 +1,13 @@
-ehco $1
-#export $SRC=~/bin/src
-#PATH=$PATH:$SRC
-#. $SRC/funs.sh
+#!/bin/bash
+
+newPath=$PATH:~/$1/src:~/$1/src/termux:~/$1/src/ubuntu:~/$1/src/utils:~/$1/src/devops:~/$1/src/mac
+export PATH=$newPath
+
+echo $newPath >~/$1/config.sh 
+echo ". ~/$1/config.sh" > ~/.bashrc
+
+if [ "$(uname -o)" = "Android" ]; then 
+	hello-termux.sh
+else
+	hello-ubuntu.sh
+fi	
