c306840d48663ec54bb1a0ab984cc528ff9293b9
Alan Hee <alan.hee@outlook.com>
2023-11-09 19:19:12 +0800

    Updated init.sh

--- src/init.sh
+++ src/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-newPath=$PATH:~/$1/src:~/$1/src/termux:~/$1/src/ubuntu:~/$1/src/utils:~/$1/src/devops:~/$1/src/mac
-export PATH=$newPath
+PATH=$PATH:~/$1/src:~/$1/src/termux:~/$1/src/ubuntu:~/$1/src/utils:~/$1/src/devops:~/$1/src/mac
+export PATH=$PATH
 
-echo $newPath >~/$1/config.sh 
+echo $PATH >~/$1/config.sh 
 echo ". ~/$1/config.sh" > ~/.bashrc
 
 if [ "$(uname -o)" = "Android" ]; then 
