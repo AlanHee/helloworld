2e0cd6064b880a6402c6d36c2520863cf3b2877e
Alan Hee <alan.hee@outlook.com>
2023-11-09 11:08:18 +0800

    Fixed update vim plugs issue

--- src/config-neovim.sh
+++ src/config-neovim.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
-source $SRCHOME/pre-load-functions.sh
 
-function getVimPlugManager() {
-	echo "Installing vim-plug manager..."
+function getPlugManager() {
 	git clone https://github.com/junegunn/vim-plug
 	mkdir -p ~/.config/nvim/autoload
 	cp -f vim-plug/plug.vim ~/.config/nvim/autoload/plug.vim
 	rm vim-plug -rf
 }
 
-if [ -e ~/.config/nvim/autoload/plug.vim ]; then
-	yesOrNo "Override existed vim-plug?(y/n)  :" getVimPlugManager
+if [ ~/.config/nvim/autoload/plug.vim ]; then
+	yesOrNo "Override existed vim-plug?(y/n)  :" getPlugManager
 fi
 
-function getVimPlugs() {
-	echo "Installing default plugs..."
+
+function getPlugs() {
 	cd $PROFILE
 	cp -f init.vim ~/.config/nvim/
-	echo 'Restored your nvim profile'
-	# update plugs
 	nvim +PlugClean[!] +PlugUpdate +qa
 }
 
-if [ -e ~/.config/nvim/init.vim ]; then
-	yesOrNo "Override existed init.vim settings?(y/n)  :" getVimPlugs
+if [ ~/.config/nvim/init.vim ]; then
+	yesOrNo "Override existed init.vim settings?(y/n)  :" getPlugs
 fi
