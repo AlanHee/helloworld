987d8d56827b3e2af26648066995e2d003b28c4b
AlanHee <alan.hee@outlook.com>
2023-11-10 19:17:22 +0800

    Fixed add user issue

--- src/config-profile-settings.sh
+++ src/config-profile-settings.sh
@@ -4,26 +4,22 @@ source funs.sh
 
 userName="AlanHee"
 userEmail="alan.hee@outlook.com"
-remoteIP="0.0.0.0"
+if [ ! -z $USER_NAME ]; then
+	userName=$USER_NAME
+fi
+if [ ! -z $USER_EMAIL ]; then
+	userEmail=$USER_EMAIL
+fi
 
-read -p "Type in your name: (default: Alan Hee)" user_name
+read -p "Type in your name: (default: $useName)" user_name
 if [ ! -z $user_name ]; then
 	userName="$user_name"
 fi
 
-read -p "Type in your email: (default: alan.hee@outlook.com)" user_email
+read -p "Type in your email: (default: $userEmail)" user_email
 if [ ! -z $user_email ]; then
 	userEmail="$user_email"
 fi
 
-# save remove ip weather with local env
-if [ "$(uname -o)" = "Android" ]; then
-	read -p "Set up remote ip($REMOTE_IP):" remote_ip
-	if [ ! -z $remote_ip ]; then
-		remoteIP=$remote_ip
-	fi
-fi
-
 saveExport USER_NAME $userName
 saveExport USER_EMAIL $userEmail
-saveExport REMOTE_IP $remoteIP
