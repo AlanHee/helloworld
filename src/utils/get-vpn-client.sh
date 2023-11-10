e22149de8ee39b9b1602b55a39f4069c2c407e14
AlanHee <alan.hee@outlook.com>
2023-11-10 20:09:38 +0800

    Fixed get-vpn-client issue

---
+++ src/utils/get-vpn-client.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source funs.sh
+source ~/.bashrc
+
+remoteIP=""
+if [ ! -z $REMOTE_IP ]; then
+	remoteIP=$REMOTE_IP
+fi
+
+function _() {
+
+	while true; do
+		read -p "type in vpn server ip(default $remoteIP): " remote_ip
+			if [ ! -z $REMOTE_IP ]; then
+				remoteIP=$remote_ip
+				break;
+			fi
+			if [ ! -z $remoteIP ]; then
+				break;
+			fi
+			echo 'ip can not be empty'
+	done
+
+	scp root@$remoteIP:/root/vpnclient.sswan ~
+	echo 'Got vpn client.'
+}
+
+yesOrNo "Get vpn client now (y/n): " _
