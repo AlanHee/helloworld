ccf4aefa835a3fdc9930bfeeb2b0ae607bef7eca
AlanHee <alan.hee@outlook.com>
2023-11-27 14:33:26 +0800

    Updated men.sh

--- src/devops/men.sh
+++ src/devops/men.sh
@@ -1,99 +1,33 @@
 #!/bin/bash
-# Shell script by alan.hee
-# email: alan.hee@outlook.com
+source funs.sh
 
 showHelp() {
 	cat <<EOF
- ${greenf}Mans on the way:
-  ns                npm run start:dev
-  ga				git add . && git commit -m $2
-  gs      			git status
-  gp  		  		git push
-  ft 				flutter test
-  fte  				flutter test -r expanded
-  fr  		  		Flutter run
-  frr				flutter run --release
-  fc				Flutter clean
-  fcr				flutter clean && flutter run
-  u   				Unlock flutter thread
-  g   				udpate flutter modules
-  fu  				flutter upgrade --force
-  fb  				build  flutter  model to Json
-  fba 				flutter build apk
-  fbi 				flutter build ios
-  oios				open adc ios project
-  fbb				flutter packages pub run build_runner build --delete-conflicting-outputs
-  e   | android-emulator	Open android emulator
-  b   | backup 			backup blog 
-  c   | backup 			backup cookbook 
-  s   | backup 			backup script
-  h   | help			Show help
-  q   | quit			Quict mans
-${reset}
+${greenf}
+dt      Dart test
+dte     Dart test -r expande
+dbb     Dart run build and delete conflicting outputs
+fr      Flutter run
+frr     Flutter run --release
+fr      Flutter clean
+fcr     Flutter clean && flutter run
+u       Flutter unlock thread
+g       Flutter update modules
+fu      Flutter upgrade --force
+fb      Flutter build model to Json
+fba     Flutter build apk
+fbi     Flutter build ios
+e       Flutter open android emulator
 EOF
-}
-
-# ANSI Color -- use these variables to easily have different color
-# and format output. Make sure to output the reset sequence after
-initializeANSI() {
-	esc=""
-
-	blackf="${esc}[30m"
-	redf="${esc}[31m"
-	greenf="${esc}[32m"
-	yellowf="${esc}[33m" bluef="${esc}[34m"
-	purplef="${esc}[35m"
-	cyanf="${esc}[36m"
-	whitef="${esc}[37m"
-
-	blackb="${esc}[40m"
-	redb="${esc}[41m"
-	greenb="${esc}[42m"
-	yellowb="${esc}[43m" blueb="${esc}[44m"
-	purpleb="${esc}[45m"
-	cyanb="${esc}[46m"
-	whiteb="${esc}[47m"
-
-	boldon="${esc}[1m"
-	boldoff="${esc}[22m"
-	italicson="${esc}[3m"
-	italicsoff="${esc}[23m"
-	ulon="${esc}[4m"
-	uloff="${esc}[24m"
-	invon="${esc}[7m"
-	invoff="${esc}[27m"
-
-	reset="${esc}[0m"
-}
-
-showDone() {
-	cat <<EOF
-${greenf}Done.${reset}
-EOF
-}
-
-backup() {
-	cd $1
-	/usr/bin/git gc
-	/usr/bin/git add .
-	/usr/bin/git commit -m "Backup taken at @ $(date)"
-	/usr/bin/git push
 }
 
 doAction() {
 	case $1 in
-	ns) npm run start:dev ;;
-	gs) git status ;;
-	gp) git gc && git push ;;
-	quit | q) exit 0 ;;
-	h | help) showHelp ;;
-	g | flutter) flutter pub get ;;
-	ft) flutter test ;;
-	fte) flutter test -r expanded $2 ;;
+	dt) dart test ;;
+	dte) dart test -r expanded $2 ;;
+	frr) flutter run --release ;;
+	dbb) dart pub run build_runner build --delete-conflicting-outputs ;;
 	fr) flutter run ;;
-	s | script) backup ~/funs ;;
-	b | blog) backup ~/blog ;;
-	c | cookbook) backup ~/cookbook ;;
 	e | android-emulator) flutter emulators --launch Pixel_3a_API_30_x86 ;;
 	u | flutter-unloack) rm ~/Documents/flutter/bin/cache/lockfile ;;
 	fb) flutter packages pub run build_runner build ;;
@@ -102,19 +36,14 @@ doAction() {
 	fc) flutter clean ;;
 	fcr) flutter clean && flutter run ;;
 	fu) flutter upgrade --force ;;
-	oios) open ios/Runner.xcworkspace ;;
-	frr) flutter run --release ;;
-	ga) git add . && git commit -m $2 ;;
-	fbb) flutter packages pub run build_runner build --delete-conflicting-outputs ;;
+	g | flutter) flutter pub get ;;
+	quit | q) exit 0 ;;
+	h | help) showHelp ;;
 	esac
 	exit 0
 }
 
-# init fun
 initializeANSI
-
-# run the cmd weather arg provided
-# else enter CLI mode
 if [ "$1" = "" ]; then
 	showHelp
 	while read command args; do
--- src/funs.sh
+++ src/funs.sh
@@ -31,10 +31,10 @@ function saveSource() {
 	if [ ! -e $SRC/config.sh ]; then
 		touch $SRC/config.sh
 	fi
-	# Update 
+	# Update
 	if grep -q "source $1" $SRC/config.sh; then
 		sed -i "s/^source $1.*/source $1/" $SRC/config.sh
-	# Add 
+	# Add
 	else
 		echo "source $1" >>$SRC/config.sh
 	fi
@@ -66,7 +66,47 @@ function yesOrNo() {
 		esac
 	done
 }
-#fixed scp error should no echo nothing  
+
+# ANSI Color -- use these variables to easily have different color
+# and format output. Make sure to output the reset sequence after
+initializeANSI() {
+	esc=""
+
+	blackf="${esc}[30m"
+	redf="${esc}[31m"
+	greenf="${esc}[32m"
+	yellowf="${esc}[33m" bluef="${esc}[34m"
+	purplef="${esc}[35m"
+	cyanf="${esc}[36m"
+	whitef="${esc}[37m"
+
+	blackb="${esc}[40m"
+	redb="${esc}[41m"
+	greenb="${esc}[42m"
+	yellowb="${esc}[43m" blueb="${esc}[44m"
+	purpleb="${esc}[45m"
+	cyanb="${esc}[46m"
+	whiteb="${esc}[47m"
+
+	boldon="${esc}[1m"
+	boldoff="${esc}[22m"
+	italicson="${esc}[3m"
+	italicsoff="${esc}[23m"
+	ulon="${esc}[4m"
+	uloff="${esc}[24m"
+	invon="${esc}[7m"
+	invoff="${esc}[27m"
+
+	reset="${esc}[0m"
+}
+
+showDone() {
+	cat <<EOF
+${greenf}Done.${reset}
+EOF
+}
+
+#fixed scp error should no echo nothing
 #scp: Received message too long 1819238756
 #scp: Ensure the remote shell produces no output for non-interactive sessions.
 #echo 'loaded funs'
