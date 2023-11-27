08932e97abe6f151948f4a295d5ad2fb22747215
AlanHee <alan.hee@outlook.com>
2023-11-27 13:29:18 +0800

    Added flutter-swith-host scripts

--- src/devops/fix-flutter-pub-get-nowork.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-## todo check shell type
-## todo check if exist same var
-## todo check if window env
-echo "PUB_HOSTED_URL=https://pub.flutter-io.cn" >>~/.zshrc
-echo "FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn" >>~/.zshrc
-echo "done."
