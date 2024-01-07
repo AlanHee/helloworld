dd234b250223af85d361859cb4726bdba4b3732c
AlanHee <alan.hee@outlook.com>
2024-01-07 17:48:17 +0800

    Added install openvpn server && client script

--- hello.sh
+++ hello.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-apt upgrade -qyy 
+apt upgrade -qyy
 apt install git -qyy
 
-read -p 'Type in a path to save scripts:' home;
+read -p 'Save script in:' home
 if [ -z ~/$home ]; then
 	$home="bin"
 fi
 if [ -e ~/$home ]; then
-	echo "Oop path existed."; 
+	echo "Oop path existed."
 	exit 0
 fi
 
-git clone https://github.com/alanhee/helloworld ~/$home;
-echo 'Got scripts.'
+git clone https://github.com/alanhee/helloworld ~/$home
+echo 'Saved script.'
 
-bash ~/$home/src/init.sh $home		
-
+bash ~/$home/src/init.sh $home
