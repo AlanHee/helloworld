768fd050ad6e403ef710fb452b0fff032fa4b588
AlanHee <alan.hee@outlook.com>
2024-01-08 16:09:12 +0800

    Modified default editor as vim-python

--- src/termux/termux-get-tools.sh
+++ src/termux/termux-get-tools.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source funs.sh
-yesOrNo "Install ssh nvim shfmt dart?(y/n):" "apt install -y openssh git vim dart shfmt"
+yesOrNo "Install ssh vim-python shfmt dart?(y/n):" "apt install -y openssh git vim-python dart shfmt"
