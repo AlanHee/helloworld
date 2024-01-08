f12fb244a8b860895470625f91706e54d8e8eda5
AlanHee <alan.hee@outlook.com>
2024-01-08 16:31:37 +0800

    Modified default install python3

--- src/termux/termux-get-tools.sh
+++ src/termux/termux-get-tools.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source funs.sh
-yesOrNo "Install ssh vim-python shfmt dart?(y/n):" "apt install -y openssh git vim-python dart shfmt"
+yesOrNo "Install ssh vim-python shfmt dart?(y/n):" "apt install -y openssh git python3 vim-python dart shfmt"
