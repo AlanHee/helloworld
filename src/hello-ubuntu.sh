543ae7f88a6030ad3998f059b6ef3e7283db4629
AlanHee <alan.hee@outlook.com>
2024-01-08 17:18:53 +0800

    Updated hello-ubunut

--- src/hello-ubuntu.sh
+++ src/hello-ubuntu.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 scripts=(
 	config-profile-settings
-	ubuntu-get-vpn-server
-	)
-scripts2=(
-	config-profile-settings
-	config-ssh-keygen
-	config-git
-	config-neovim
-	ubuntu-get-vpn-server
-	ubuntu-get-dart
-	ubuntu-get-dart-frog
-	ubuntu-get-flutter
-	ubuntu-get-docker
-	ubuntu-get-nosql-database
+	ubuntu-get-openvpn
 )
-
-scripts1=(
-	ubuntu-get-vpn-server
-	ubuntu-get-tools
-	config-profile-settings
-	config-git
-	config-neovim
-	config-ssh-keygen
-	ubuntu-get-dart
-	ubuntu-get-dart-frog
-	ubuntu-get-bt-panel
-)
+# scripts=(
+# 	config-profile-settings
+# 	config-ssh-keygen
+# 	config-git
+# 	config-vim
+# 	ubuntu-get-vpn-server
+# 	ubuntu-get-dart
+# 	ubuntu-get-dart-frog
+# 	ubuntu-get-flutter
+# 	ubuntu-get-docker
+# 	ubuntu-get-nosql-database
+# )
 
 for script in ${scripts[@]}; do
 	bash $script.sh
