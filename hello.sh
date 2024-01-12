e2b2c5a13d58338160ec9b9405b602b71c2284fa
AlanHee <alan.hee@outlook.com>
2024-01-12 20:52:37 +0800

    Updated keymapper profile

--- hello.sh
+++ hello.sh
@@ -2,17 +2,17 @@
 
 apt upgrade -qyy
 
-read -p 'Save script in:' home
+read -p 'Let script in:' home
 if [ -z ~/$home ]; then
 	$home="bin"
 fi
 if [ -e ~/$home ]; then
-	echo "Oop path existed."
+	echo "Oop, path existed."
 	exit 0
 fi
 
 apt install git -qyy
 git clone https://github.com/alanhee/helloworld ~/$home
-echo 'Saved script.'
+echo 'Got script.'
 
 bash ~/$home/src/init.sh $home
