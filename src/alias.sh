407052e2afaaa595bacc3836929e612bdb983056
AlanHee <alan.hee@outlook.com>
2024-02-08 18:20:51 +0800

    Added alais gat: git just commit with date time

--- src/alias.sh
+++ src/alias.sh
@@ -5,6 +5,7 @@ alias gs='git status'
 alias sm='show-menu.sh | less'
 alias ss='show-status.sh'
 alias cs='connect-to-server.sh'
+alias gat="ga $(date)"
 
 #fixed scp error should no echo nothing
 #scp: Received message too long 1819238756
