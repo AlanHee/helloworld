49b1be471699215986599e2b664faa3d5ae80419
AlanHee <alan.hee@outlook.com>
2024-02-10 18:27:48 +0800

    Update hello-ubuntu.sh

--- src/hello-ubuntu.sh
+++ src/hello-ubuntu.sh
@@ -2,20 +2,15 @@
 scripts=(
 	config-user-name
 	config-user-email
+	config-ssh-keygen
+	config-git
+	config-vim
 	ubuntu-get-openvpn
+	ubuntu-get-dart
+	ubuntu-get-dart-frog
+	ubuntu-get-flutter
+	ubuntu-get-docker
 )
-# scripts=(
-# 	config-profile-settings
-# 	config-ssh-keygen
-# 	config-git
-# 	config-vim
-# 	ubuntu-get-vpn-server
-# 	ubuntu-get-dart
-# 	ubuntu-get-dart-frog
-# 	ubuntu-get-flutter
-# 	ubuntu-get-docker
-# 	ubuntu-get-nosql-database
-# )
 
 for script in ${scripts[@]}; do
 	bash $script.sh
