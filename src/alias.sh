fab73eba01f6ed5d5752c0c0d5e698d0c7a79e92
AlanHee <alan.hee@outlook.com>
2024-05-09 03:18:58 +0800

    Added more alias

--- src/alias.sh
+++ src/alias.sh
@@ -5,7 +5,9 @@ alias gs='git status'
 alias sm='show-menu.sh'
 alias ss='show-status.sh'
 alias cs='connect-to-server.sh'
+alias ss='sync-to-server.sh'
 alias gat="ga $(date)"
+alias pub="dart pub"
 
 #fixed scp error should no echo nothing
 #scp: Received message too long 1819238756
