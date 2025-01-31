a537914bbadbe247d31a867787ddb493ff6e7cd1
AlanHee <alan.hee@outlook.com>
2025-01-31 12:10:23 +0800

    Fri

--- src/get-more-scripts.sh
+++ src/get-more-scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source funs.sh
 
-list=(mac utils devops ubuntu cookbook config)
+list=(mac utils devops ubuntu cookbook blockchain config)
 for i in "${!list[@]}"; do
 	echo "[$((i + 1))] ${list[$i]}"
 done
