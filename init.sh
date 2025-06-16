d90b9190c7dc5cb955aa96ef976ad08790fbd3be
Alan <alan.hee@outlook.com>
2025-06-16 13:00:04 +0800

    Update init.sh
    Fixed exist issue

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-read -p 'Get shell script in path(default as ~/bin): ' home
+read -p 'Save script in (default ~/bin): ' home
 if [ -z $home ]; then
 	home="bin"
 fi
 if [ -e ~/$home ]; then
 	echo "Oop, path existed."
-	exit 0
+	exit 1
 fi
 
 if [ "$(uname -o)" == "Android" ]; then
@@ -15,10 +15,12 @@ fi
 
 apt upgrade -qy
 apt install git -qy
+
 if git clone --progress https://github.com/alanhee/helloworld ~/$home; then
 	echo "Cloned success in: ~/$home"
 else
 	echo 'Cloned failed. Check your network.'
+ exit 1
 fi
 
 bash ~/$home/src/hello.sh $home
